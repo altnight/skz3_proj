fbba06064b999b375449e83db597f21540aa3dc7
atasatamatara <atasatamatara@gmail.com>
2013-03-11 21:52:59 +0900

    fix

--- a.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-git filter-branch --commit-filter 'GIT_AUTHOR_NAME="atasatamatara" GIT_AUTHOR_EMAIL="atasatamatara@gmail.com" GIT_COMMITTER_NAME="atasatamatara" GIT_COMMITTER_EMAIL="atasatamatara@gmail.com" git commit-tree "$@"' HEAD
